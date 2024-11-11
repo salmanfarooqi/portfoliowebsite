7c7c8cdca026ec6d10cbe98fc1ba54b577248e8b
salman <salmanfarooqi1272001@gmail.com>
2024-11-11 09:06:25 -0800

    commit

--- src/components/newcomponents/HomeSilder.tsx
+++ src/components/newcomponents/HomeSilder.tsx
@@ -19,8 +19,8 @@
 
     const downloadFile = () => {
       const link = document.createElement("a");
-      link.href = "/Document/salman cv.pdf";
-      link.download = "document.pdf";
+      link.href = "/Document/salmn.docx";
+      link.download = "document.docx";
       document.body.appendChild(link);
       link.click();
       document.body.removeChild(link);
